7499b3eacbdf20bdce9d5e0b4bfecb95c5f48c59
Alexey Slapoguzov <alex.slapoguzov@gmail.com>
2018-07-09 17:43:36 +0300

    commit all changes

--- src/test/java/com/issoft/training/pages/LoginPage.java
+++ src/test/java/com/issoft/training/pages/LoginPage.java
@@ -1,4 +1,34 @@
 package com.issoft.training.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    private WebDriver driver;
+
+    @FindBy(id = "Username")
+    private WebElement loginField;
+
+    @FindBy(id = "Password")
+    private WebElement passwordField;
+
+    @FindBy(css = ".button.loginButton.gradientforbutton")
+    private WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public MailPage login(String login, String password) {
+        loginField.clear();
+        loginField.sendKeys(login);
+        passwordField.clear();
+        passwordField.sendKeys(password);
+        loginButton.click();
+        return new MailPage(driver);
+    }
 }
